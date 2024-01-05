ad736576556d99e48fb6cb78e44f9875261943ea
beny81 <152813531+beny81@users.noreply.github.com>
2024-01-05 15:16:35 -0500

    Create ajax.js

---
+++ Starline/js/ajax.js
@@ -0,0 +1,36 @@
+( function(window){
+
+
+    const ajax={};
+
+    function getNewRequest () {
+        if( window.XMLHttpRequest){
+            return (new XMLHttpRequest());
+            }
+        else {
+            window.alert("Ajax is not available");
+            return(null);
+            }
+        }
+
+    ajax.sendGetRequest= function (requestURL, responseHandler){
+        const xhr = getNewRequest();
+        xhr.onreadystatechange = function () {
+            handleResponse(xhr, responseHandler);
+             };
+
+        xhr.open("GET",requestURL, true);
+        xhr.responseType= "text";
+        xhr.send();
+     };
+    
+    function handleResponse(xhr, responseHandler){
+        if((xhr.readystate== 4) && (xhr.status==200)){
+            
+            responseHandler(xhr);
+         }
+      }
+    window.ajax= ajax;
+    
+
+})(window);
