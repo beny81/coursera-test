831879c8a0e1c19512a41fe3ac774cb7c022e474
beny81 <152813531+beny81@users.noreply.github.com>
2024-01-04 20:56:00 -0500

    Update script.js

--- Starline/js/script.js
+++ Starline/js/script.js
@@ -3,8 +3,17 @@ document.addEventListener("DOMContentLoaded",
               document.querySelector("button")
                       .addEventListener("click", sayhello);
               function sayhello (event){
-                      let texte = document.getElementById("texte").value;
-                      texte = "Sorry we do not have '" + texte + "' on this platform"
+                      let texte = document.getElementById("texte").value;    
+                      if (texte==="2017-02-09"){
+                          texte="Your first ever kiss";
+                          }
+                      else if (texte==="1999-10-18"){
+                          texte="Big Mike was born";
+                          }
+                       else{
+                            texte = "Sorry we do not have '" + texte + "' on this platform";
+                           }
+                        
                       document.querySelector("div")
                               .textContent = texte;    
            
