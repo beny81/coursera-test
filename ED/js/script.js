81bea870ef9cf3893c80378647c46df502293990
beny81 <152813531+beny81@users.noreply.github.com>
2024-01-08 08:42:36 -0500

    Update script.js

--- ED/js/script.js
+++ ED/js/script.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", 
-      function (event){let b=1; 
+      function (event){ 
               document.querySelector(".navbar-toggler")
-                      .addEventListener("click", function(event){
+                      .addEventListener("blur", function(event){
                       let screenWidth = window.innerWidth;
-                         b=0;
-                          document.querySelector("#main")
-                      .addEventListener("click", function(event){
-                          
+                        
+                       
                      
-                      if(screenWidth<769 && b===0){
+                      if(screenWidth<769){
                          
-                           screenWidth= document.querySelector(".navbar-toggler") .click();
-                             b=1;       
+                         document.querySelector(".navbar-toggler") .click();
+                                 
                           }
                         
            
                       });
-               });
+              
            });
