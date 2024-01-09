52013691b2f28739ec2c9863bf31fd3b260376e8
beny81 <152813531+beny81@users.noreply.github.com>
2024-01-09 13:37:01 -0500

    Update script.js

--- ED/js/script.js
+++ ED/js/script.js
@@ -1,24 +1,49 @@
 document.addEventListener("DOMContentLoaded", 
       function (event){ 
           
-        
-              document.querySelector(".navbar-toggler")
-                      .addEventListener("focus", function(event){let b=3;
-                         
-                           document.querySelector("*")
+         let nav=2;
+              document.querySelector("*")
                       .addEventListener("click", function(event){
-                      let screenWidth = window.innerWidth;
+
+                            
+                         document.querySelector(".navbar-toggler")
+                      .addEventListener("click", function(event){
+                          
+                          if(nav==3){
+                              nav=3;
+                                }
+                          else{
+                              nav=2; 
+                          }
+                         
+                        }); 
+
+                                if ( nav==3){
+
+                                  let screenWidth = window.innerWidth;
+                         
                      
-                      if(screenWidth<769 && b>0){
+                                  if(screenWidth<769 ){
                          
-                         document.querySelector(".navbar-toggler") .click();
-                          b-=1;
-                         
+                                   document.querySelector(".navbar-toggler") .click();
+                                  
+                                       console.log("hello1");
+                                     
+                                     
+                          
                                  
                           }
                         
-           
+
+                        } 
+                           nav++;
+                      
+                       console.log("hello2");
                       });
-                    });       
-              
-           });
+                         
+       });
+// var showLoading = function (selector) {
+//   var html = "<div class='text-center'>";
+//   html += "<img src='images/ajax-loader.gif'></div>";
+//   insertHtml(selector, html);
+// };
